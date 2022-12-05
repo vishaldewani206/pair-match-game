c1a177b4f241eed3790a813b449fcf29293f609e
Vishal <dewani.vishal206@gmail.com>
2022-12-05 20:59:10 +0500

    complete

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let cards = document.querySelectorAll('.card');
+let cardsContainer = document.querySelector('.cards');
+let wrongGuessP = document.querySelector('.wrong');
 let imageNum = Math.floor(Math.random() * 5);
 let display = true;
 let lastI;
 let cardsValue = [];
-let firstValue;
-let secondValue;
-
+let clickedValue;
+let cardNum;
+let wrongGuess = 0;
+let startBtn = document.querySelector('#start');
 //for getting value of cards and storing it into cardsValue
 for (let j = 0; j < cards.length; j++) {
   cardsValue.push(cards[j].getAttribute('value'));
@@ -17,13 +20,36 @@ for (let i = cardsValue.length - 1; i > 0; i--) {
   cardsValue[i] = cardsValue[j];
   cardsValue[j] = temp;
 }
-//for changing innerHtml of cards div
-for (let i = 0; i < cardsValue.length; i++) {
-  cards[i].innerHTML = cardsValue[i];
-}
+//start button
+startBtn.addEventListener('click', () => {
+  startBtn.style.display = 'none';
+  cardsContainer.style.display = 'grid';
+});
 console.log(cardsValue);
 
 //main function of cards diplay
 for (let i = 0; i < cards.length; i++) {
-  cards[i].onclick = function () {};
+  cards[i].onclick = function () {
+    if (display) {
+      cards[i].style.backgroundImage = 'url("' + cardsValue[i] + '.jpg")';
+      clickedValue = cardsValue[i];
+      cardNum = i;
+      console.log(clickedValue);
+      display = false;
+    } else {
+      if (clickedValue == cardsValue[i]) {
+        cards[i].style.backgroundImage = 'url("' + cardsValue[i] + '.jpg")';
+        display = true;
+        cardNum = i;
+        clickedValue = cardsValue[i];
+      } else {
+        cards[i].style.backgroundImage = 'url("' + cardsValue[i] + '.jpg")';
+        cards[cardNum].style.backgroundImage = '';
+        cardNum = i;
+        clickedValue = cardsValue[i];
+        wrongGuess++;
+        wrongGuessP.innerHTML = `Wrong Guesses: ${wrongGuess}`;
+      }
+    }
+  };
 }
